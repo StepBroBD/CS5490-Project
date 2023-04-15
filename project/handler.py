a3f262ebfee630d2b7b6369d23914ae184027f34
Gates Lamb <gatesalamb@gmail.com>
2023-04-15 12:48:30 -0600

    test

--- project/handler.py
+++ project/handler.py
@@ -4,6 +4,5 @@ import socketserver
 class RequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print(self.request)
         print(f"Received {self.data} from {self.client_address[0]}")
         self.request.sendall(self.data.upper())
