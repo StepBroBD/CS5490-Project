dbf55a2745507f1555597738a31b260552faabb1
StepBroBD <Hi@StepBroBD.com>
2023-04-15 12:43:49 -0600

    project: common send and recv

--- project/commands/start.py
+++ project/commands/start.py
@@ -2,6 +2,7 @@ import click
 import socket
 import socketserver
 from project.handler import RequestHandler
+from project.common import send_all, recv_all
 from rich import print
 
 
@@ -33,3 +34,30 @@ def server(host: str, port: int) -> None:
     print(f"Listening on {host}:{port}...")
     with socketserver.TCPServer((host, port), RequestHandler) as server:
         server.serve_forever()
+
+
+@start.command()
+@click.option(
+    "--host",
+    "-h",
+    required=True,
+    type=str,
+)
+@click.option(
+    "--port",
+    "-p",
+    required=True,
+    type=int,
+)
+def client(host: str, port: int) -> None:
+    """
+    Start the client.
+    """
+    # test client
+    # send a hello world message to the server
+    # and close the connection
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        send_all(s, b"Hello World")
+        data = recv_all(s)
+        print(f"Received {data} from {host}:{port}")
--- project/handler.py
+++ project/handler.py
@@ -5,4 +5,4 @@ class RequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print(f"Received {self.data} from {self.client_address[0]}")
-        self.request.sendall(self.data.upper())
+        self.request.sendall(self.data)
