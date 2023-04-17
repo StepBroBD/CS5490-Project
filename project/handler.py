0d532c7038bfaefa0ae487442b783d8896b44276
Gates Lamb <gatesalamb@gmail.com>
2023-04-16 18:11:51 -0600

    edited handler.handle

--- project/handler.py
+++ project/handler.py
@@ -1,8 +1,15 @@
-import socketserver
 
+import socketserver
+import project.common
 
 class RequestHandler(socketserver.BaseRequestHandler):
+
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print(f"Received {self.data} from {self.client_address[0]}")
+        self.data = project.common.recv_all(self.request).decode()
+        httpRequest = self.data.split('\r\n')
+        requestInfo = httpRequest[0].split(' ')
+        method = requestInfo[0]
+        endpoint = requestInfo[1]
+        #print(httpRequest)
+        print(method + ' ' + endpoint)
         self.request.sendall(self.data)
