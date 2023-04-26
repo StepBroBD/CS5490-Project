f2ffefe08a52e66d8bf708d78c295096699df59d
Sam Smith <samsmithe20@gmail.com>
2023-04-25 19:31:00 -0600

    added blocking to handler

--- project/handler.py
+++ project/handler.py
@@ -2,6 +2,19 @@ import socketserver
 from typing import Tuple
 from project.common import recv_all, send_all
 from rich import print
+import time
+import math
+
+# dictionary of requests
+requests = {}
+size_counts = {}
+times = {}
+start = time.time()
+
+blacklist = []
+
+window_size = 2
+block_frequency = 0.2
 
 
 class RequestHandler(socketserver.BaseRequestHandler):
@@ -13,10 +26,59 @@ class RequestHandler(socketserver.BaseRequestHandler):
         self.method, self.path, self.version = parse_request(self.data)
         self.headers = parse_headers(self.data)
 
-        self.log(f"Headers: {self.headers}")
-        self.request.sendall(form_response(
-            self.method, self.path, self.version, False, "Hello World"
-        ))
+        # self.log(f"Headers: {self.headers}")
+
+        # length of headers in bytes for analysis
+        headers_length = len(self.headers['user-agent']) + len(self.headers['sec-ch-ua']) + len(self.headers['accept'])
+        # add request to dictionary
+        requests[self.client_address[0] + ":" + str(self.client_address[1])] = headers_length
+
+        print(math.trunc(time.time() - start) % 3)
+
+        # check blacklist
+        if headers_length in blacklist:
+            self.request.sendall(form_response(
+                self.method, self.path, self.version, True, "Go Away"
+            ))
+        else:
+            # add header lengths to dictionary
+            if headers_length in size_counts.keys():
+                size_counts[headers_length] = size_counts[headers_length] + 1
+                if math.trunc(time.time() - start) % 3 == 0:
+                    for c in size_counts.keys():
+                        if size_counts[c] > 3:
+                            blacklist.append(c)
+                    size_counts.clear()
+            else:
+                # haven't seen before send ok response
+                size_counts[headers_length] = 1
+                times[headers_length] = time.time()
+            self.request.sendall(form_response(
+                self.method, self.path, self.version, False, "Hello World"
+            ))
+
+            # if size_counts[headers_length] % window_size == 0:
+            #     if time.time() - times[headers_length] < block_frequency:
+            #         print("blocked")
+            #         # block
+            #         self.request.sendall(form_response(
+            #             self.method, self.path, self.version, True, "Go Away"
+            #         ))
+            #     else:
+            #         print("allowed")
+            #         # allow
+            #         times[headers_length] = time.time()
+            #         self.request.sendall(form_response(
+            #             self.method, self.path, self.version, False, "Hello World"
+            #         ))
+            # else:
+            #     self.request.sendall(form_response(
+            #         self.method, self.path, self.version, False, "Hello World"
+            #     ))
+        # if len(requests.keys()) >= 1000:
+            # reset and write to file
+        # self.request.sendall(form_response(
+        #     self.method, self.path, self.version, True, "Hello World"
 
 
 def parse_request(data: bytes) -> Tuple[str, str, str]:
@@ -43,10 +105,18 @@ def form_response(method: str, path: str, version: str, blocked: bool, message: 
     Form a response from the request
     """
     if blocked:
+        # original
+        # response = f"{version} 403 Forbidden\r\n"
+        # response += "Content-Type: text/html\r\n"
+        # response += "Content-Length: 0\r\n"
+        # response += "\r\n"
+
+        # modified
         response = f"{version} 403 Forbidden\r\n"
         response += "Content-Type: text/html\r\n"
-        response += "Content-Length: 0\r\n"
+        response += f"Content-Length: {len(message)}\r\n"
         response += "\r\n"
+        response += message
     else:
         response = f"{version} 200 OK\r\n"
         response += "Content-Type: text/html\r\n"
